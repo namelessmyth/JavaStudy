126383de9ba04f5ec5bacfe6be53a2b1104a322a
namelessmyth <namelessmyth@163.com>
2023-12-29 18:49:28 +0800

    复习leetcode题目，148，142

--- algorithm/src/main/java/com/gem/leetcode/Lee142.java
+++ algorithm/src/main/java/com/gem/leetcode/Lee142.java
@@ -7,34 +7,59 @@ import java.util.Set;
 
 /**
  * <a href="https://leetcode.cn/problems/linked-list-cycle-ii/">
- * 142. 环形链表
+ * 142. 环形链表（中等）
  * </a><br>
  *
  * @author namelessmyth
  * @version 1.0
  */
 public class Lee142 {
+    /**
+     * <ol>
+     *     <li>定义快慢指针，快指针的速度是慢指针的2倍。如果存在环则他们一定会相遇</li>
+     *     <li>根据数学公式可以推倒，快指针从出发点到入口的距离和相遇点到入口的距离是相等的。</li>
+     *     <li>所以此时让快指针定位到出发点，两个指针用相同速度往前走</li>
+     *     <li>他们的相遇点就是入环的节点</li>
+     * </ol>
+     * @param head
+     * @return
+     */
     public ListNode detectCycle(ListNode head) {
-        if(head != null && head.next != null){
-            ListNode fast = head;
-            ListNode slow = head;
+        if (head != null && head.next != null) {
+            //定义快慢指针
+            ListNode fast = head, slow = head;
             while (fast != null && fast.next != null) {
+                //快指针一次跑2格，慢指针一次跑1格
                 fast = fast.next.next;
                 slow = slow.next;
+                // 如果存在环，则他们必然会相遇
                 if (fast == slow) {
                     //快慢指针相遇后，将快指针重定向到起点。
                     fast = head;
+                    //快指针从出发点到入口的距离应该是和慢指针从相遇点到入口的距离相等。
                     while (fast != slow) {
+                        //快慢指针用一样的速度走。相遇就代表到达入口
                         fast = fast.next;
                         slow = slow.next;
                     }
                     return fast;
                 }
             }
+            //走到这里代表链表没有环，快指针先走完。
         }
         return null;
     }
 
+    /**
+     * <ol>
+     *     <li>使用hashset记录指针走过的每一个节点，注意：不是记录节点的值。</li>
+     *     <li>如果链表不存在环，则循环的时候，则一遍就循环完了</li>
+     *     <li>如果链表存在环，那一定会循环到一个hastset中已经存在的节点</li>
+     *     <li>第一个重复的节点就是入环节点，返回即可</li>
+     * </ol>
+     * @param head
+     * @return
+     */
     public ListNode detectCycle3ms(ListNode head) {
         ListNode pos = head;
         Set<ListNode> visited = new HashSet<ListNode>();
--- algorithm/src/main/java/com/gem/leetcode/Lee148.java
+++ algorithm/src/main/java/com/gem/leetcode/Lee148.java
@@ -2,13 +2,10 @@ package com.gem.leetcode;
 
 import com.gem.leetcode.list.ListNode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
- * <a href="https://leetcode.cn/problems/linked-list-cycle-ii/">
+ * <a href="https://leetcode.cn/problems/sort-list/">
  * 148. 排序链表（中等）
  * </a><br>
  *
@@ -17,7 +14,13 @@ import java.util.Set;
  */
 public class Lee148 {
     /**
-     * 注意：这个方法是用空间换的时间。虽然速度能够达到2ms，但空间复杂度比较高。55.12MB, 击败24.78%使用 Java 的用户
+     * 注意：这个方法是用空间换的时间。虽然速度能够达到2ms，但空间复杂度比较高。
+     * <ol>
+     *     <li>第1次循环，找到链表的最大值和最小值，构建一个数组，大小为最大值-最小值+1</li>
+     *     <li>第2次循环，在数组中记录每一个数值出现的次数，数组下标为：节点的值-最小值</li>
+     *     <li>第3次循环，循环数组，只要当前数组值大于0就修改链表当前个节点的值</li>
+     *     <li>改完之后，数组当前值-1，链表跳到下一个节点，直到当前值为0，继续数组下一个值。</li>
+     * </ol>
      */
     public ListNode sortList(ListNode head) {
         ListNode temp = head;
@@ -56,6 +59,36 @@ public class Lee148 {
         return dummy.next;
     }
 
+    public ListNode my(ListNode head) {
+        //定义最大值和最小值
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        //第1次循环，找到链表的最大值和最小值，构建一个数组，大小为最大值-最小值+1
+        ListNode temp = head;
+        while (temp != null) {
+            max = Math.max(temp.val, max);
+            min = Math.min(temp.val, min);
+            temp = temp.next;
+        }
+        //第2次循环，在数组中记录每一个数值出现的次数，数组下标为：节点的值-最小值
+        int[] count = new int[max - min + 1];
+        temp = head;
+        while (temp != null) {
+            count[temp.val - min]++;
+            temp = temp.next;
+        }
+        //第3次循环，循环数组，只要当前数组值大于0就修改链表当前个节点的值
+        temp = head;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                //改完之后，数组当前值-1，链表跳到下一个节点，直到当前值为0，继续数组下一个值。
+                temp.val = i + min;
+                temp = temp.next;
+                count[i]--;
+            }
+        }
+        return head;
+    }
+
     /**
      * 将入参链表转换成ArrayList，使用jdk自带方法排序
      */
@@ -78,8 +111,24 @@ public class Lee148 {
         return list.get(0);
     }
 
+    public ListNode myList(ListNode head) {
+        ListNode temp = head;
+        List<Integer> list = new ArrayList<>();
+        while (temp != null) {
+            list.add(temp.val);
+            temp = temp.next;
+        }
+        Collections.sort(list);
+        temp = head;
+        for (Integer i : list) {
+            temp.val = i;
+            temp = temp.next;
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
-        ListNode head = new ListNode(new int[]{3, 2, 0, -4});
-        System.out.println(new Lee148().sortList(head));
+        ListNode head = new ListNode(new int[]{-1, 5, 3, 4, 0});
+        System.out.println(new Lee148().my(head));
     }
 }
